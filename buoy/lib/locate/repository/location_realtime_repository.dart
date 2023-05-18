db65b3c7f54cd053a9ed318daae079c35bdac332
flutterWithChris <94571307+flutterWithChris@users.noreply.github.com>
2023-05-18 16:15:41 -0400

    Delete Last Location

--- buoy/lib/locate/repository/location_realtime_repository.dart
+++ buoy/lib/locate/repository/location_realtime_repository.dart
@@ -8,6 +8,18 @@ class LocationRealtimeRepository {
     required double latitude,
     required double longitude,
   }) async {
+    // Delete the existing location data
+    await supabase
+        .from('location_updates')
+        .delete()
+        .eq('user_id', _supabase.auth.currentUser!.id);
+
+    // TODO: Handle errors
+    // if (deleteResponse.error != null) {
+    //   print(
+    //       'Error deleting old location data: ${deleteResponse.error!.message}');
+    //   return;
+    // }
     final response = await _supabase.from('location_updates').insert([
       {
         'user_id': _supabase.auth.currentUser!.id,
